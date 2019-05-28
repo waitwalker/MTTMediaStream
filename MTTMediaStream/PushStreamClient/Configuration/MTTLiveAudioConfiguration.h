d982ab23728f0563745b8d7ee5d3781e20400737
liuchuanan <waitwalker@163.com>
2019-05-28 15:52:00 +0800

    audio configuration init

---
+++ MTTMediaStream/PushStreamClient/Configuration/MTTLiveAudioConfiguration.h
@@ -0,0 +1,19 @@
+//
+//  MTTLiveAudioConfiguration.h
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/5/28.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+
+@interface MTTLiveAudioConfiguration : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTTMediaStream/PushStreamClient/Configuration/MTTLiveAudioConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  MTTLiveAudioConfiguration.m
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/5/28.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import "MTTLiveAudioConfiguration.h"
+
+@implementation MTTLiveAudioConfiguration
+
+@end
