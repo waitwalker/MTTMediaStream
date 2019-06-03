4a626bc1aea81a5c5b19a389ca7c6229d8e4719a
liuchuanan <waitwalker@163.com>
2019-06-03 11:30:47 +0800

    methods and properties added

--- MTTMediaStream/PushStreamClient/1.Capture/MTTAudioCapture.h
+++ MTTMediaStream/PushStreamClient/1.Capture/MTTAudioCapture.h
@@ -7,10 +7,48 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "MTTLiveAudioConfiguration.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// componentFailed notification post name
+extern NSString * _Nullable const kAudioComponentFailedToCreateNotification;
+
+// MARK: - audio capture delegate
+@class MTTAudioCapture;
+@protocol MTTAudioCaptureDelegate <NSObject>
+
+/**
+ 音频采集回调
+
+ @param audioCapture 采集对象
+ @param audioData 采集到的数据
+ */
+- (void)captureOutput:(nullable MTTAudioCapture *)audioCapture audioData:(nullable NSData *)audioData;
+
+@end
+
 @interface MTTAudioCapture : NSObject
+// delegate
+@property (nonatomic, weak) id<MTTAudioCaptureDelegate>delegate;
+
+// 静音
+@property (nonatomic, assign) BOOL muted;
+
+// 是否正在采集
+@property (nonatomic, assign) BOOL running;
+
+- (instancetype)init UNAVAILABLE_ATTRIBUTE;
++ (instancetype)new UNAVAILABLE_ATTRIBUTE;
+
+
+/**
+ 根据音频配置生成实例
+
+ @param configuration 音频配置
+ @return 实例
+ */
+- (instancetype)initWithAudioConfiguration:(nullable MTTLiveAudioConfiguration *)configuration;
 
 @end
 
