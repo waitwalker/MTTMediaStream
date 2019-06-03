341518ec5d7b3f573bcba97784094cae3462d4e4
liuchuanan <waitwalker@163.com>
2019-06-03 11:17:36 +0800

    empty filter added

---
+++ MTTMediaStream/PushStreamClient/2.Render/MTTGPUImageEmptyFilter.h
@@ -0,0 +1,19 @@
+//
+//  MTTGPUImageEmptyFilter.h
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/6/3.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GPUImage/GPUImage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+@interface MTTGPUImageEmptyFilter : GPUImageFilter
+
+@end
+
+NS_ASSUME_NONNULL_END
