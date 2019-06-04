1fcaa2b546bd2d0df2bebf25ca093065be63c42c
liuchuanan <waitwalker@163.com>
2019-06-04 17:05:22 +0800

    aduio encode interface define

--- MTTMediaStream/PushStreamClient/3.Encoder/MTTAudioEncodeInterface.h
+++ MTTMediaStream/PushStreamClient/3.Encoder/MTTAudioEncodeInterface.h
@@ -8,17 +8,61 @@
 
 #import <Foundation/Foundation.h>
 #import "MTTLiveAudioConfiguration.h"
+#import <AVFoundation/AVFoundation.h>
+#import "MTTAudioFrame.h"
 
 NS_ASSUME_NONNULL_BEGIN
+
+@protocol MTTAudioEncodeDelegate <NSObject>
+
+@required
+
+/**
+ 音频编码回调
+
+ @param encoder 编码器
+ @param frame 音频编码后的帧
+ */
+- (void)audioEncoder:(nullable id<MTTAudioEncodeInterface>)encoder audioFrame:(MTTAudioFrame *)frame;
+
+@end
 
 @protocol MTTAudioEncodeInterface <NSObject>
 
 @required
+/**
+开始音频编码
+
+ @return void
+ */
 - (void)encodeAudioData:(nullable NSData*)audioData timeStamp:(uint64_t)timeStamp;
+
+/**
+ 停止音频编码
+ */
 - (void)stopEncoder;
 
 @optional
+
+
+/**
+ 根据音频配置生成音频编码实例
+
+ @param configuration 音频配置
+ @return instance
+ */
 - (nullable instancetype)initWithAudioSteamConfiguration:(nullable MTTLiveAudioConfiguration *)configuration;
+
+
+/**
+ 设置delegate
+
+ @param delegate delegate
+ */
+- (void)setDelegate:(nullable id<MTTAudioEncodeDelegate>)delegate;
+
+
+- (nullable NSData*)adtsData:(NSInteger)channel rawDataLength:(NSInteger)rawDataLength;
 
 @end
 
