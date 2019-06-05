470f534c460f5950674d71b646d230c28e971c73
liuchuanan <waitwalker@163.com>
2019-06-05 08:55:09 +0800

    H264 encoder class added

---
+++ MTTMediaStream/PushStreamClient/3.Encoder/MTTH264VideoEncoder.h
@@ -0,0 +1,17 @@
+//
+//  MTTH264VideoEncoder.h
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/6/5.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTTH264VideoEncoder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTTMediaStream/PushStreamClient/3.Encoder/MTTH264VideoEncoder.m
@@ -0,0 +1,13 @@
+//
+//  MTTH264VideoEncoder.m
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/6/5.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import "MTTH264VideoEncoder.h"
+
+@implementation MTTH264VideoEncoder
+
+@end
