44438dbe94c582d35a5a2e2dae3fdd5834be3d15
liuchuanan <waitwalker@163.com>
2019-06-05 15:31:26 +0800

    converter

--- MTTMediaStream/PushStreamClient/3.Encoder/MTTHardwareAudioEncoder.m
+++ MTTMediaStream/PushStreamClient/3.Encoder/MTTHardwareAudioEncoder.m
@@ -44,6 +44,69 @@
     return self;
 }
 
+- (void)dealloc {
+    if (accBuf) {
+        free(accBuf);
+    }
+    if (leftBuf) {
+        free(leftBuf);
+    }
+}
+
+- (void)setDelegate:(id<MTTAudioEncodeDelegate>)delegate {
+    _accDelegate = delegate;
+}
+
+- (void)encodeAudioData:(NSData *)audioData timeStamp:(uint64_t)timeStamp {
+    if (<#condition#>) {
+        <#statements#>
+    }
+}
+
+- (BOOL)createAudioConvert {
+    if (m_converter != nil) {
+        return true;
+    }
+    
+    AudioStreamBasicDescription inputFormat = {0};
+    inputFormat.mSampleRate = _configuration.audioSampleRate;
+    inputFormat.mFormatID = kAudioFormatLinearPCM;
+    inputFormat.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsPacked;
+    inputFormat.mChannelsPerFrame = (UInt32)_configuration.numberOfChannels;
+    inputFormat.mFramesPerPacket = 1;
+    inputFormat.mBitsPerChannel = 16;
+    inputFormat.mBytesPerFrame = inputFormat.mBitsPerChannel / 8 * inputFormat.mChannelsPerFrame;
+    inputFormat.mBytesPerPacket = inputFormat.mBytesPerFrame * inputFormat.mFramesPerPacket;
+    
+    // 输出音频格式
+    AudioStreamBasicDescription outputFormat;
+    memset(&outputFormat, 0, sizeof(outputFormat));
+    outputFormat.mSampleRate = inputFormat.mSampleRate;//采样率保持一致
+    outputFormat.mFormatID = kAudioFormatMPEG4AAC;//AAC 编码
+    outputFormat.mChannelsPerFrame = (UInt32)_configuration.numberOfChannels;
+    outputFormat.mFramesPerPacket = 1024;//AAC 一帧是1024字节
+    const OSType subtype = kAudioFormatMPEG4AAC;
+    AudioClassDescription requestCodecs[2] = {
+        {
+            kAudioEncoderComponentType,
+            subtype,
+            kAppleSoftwareAudioCodecManufacturer
+        },
+        {
+            kAudioEncoderComponentType,
+            subtype,
+            kAppleHardwareAudioCodecManufacturer
+        }
+    };
+    OSStatus result = AudioConverterNewSpecific(&inputFormat, &outputFormat, 2, requestCodecs, &m_converter);
+    UInt32 outputBitrate = _configuration.audioBitRate;
+    UInt32 propSize = sizeof(outputBitrate);
+    if (result == noErr) {
+        result = AudioConverterSetProperty(m_converter, kAudioConverterEncodeBitRate, propSize, &outputBitrate);
+    }
+    return true;
+}
+
 - (void)initForFilePath {
     NSString *path = [self getFilePathWithFileName:@"iOSAudioDemo.acc"];
     NSLog(@"%@",path);
