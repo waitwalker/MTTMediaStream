6c7138fc5b52d278e66d3d77085d8c75d25e9d65
liuchuanan <waitwalker@163.com>
2019-06-06 09:56:56 +0800

    stream info class added

---
+++ MTTMediaStream/PushStreamClient/4.PushStream/Model/MTTLiveStreamInfo.m
@@ -0,0 +1,13 @@
+//
+//  MTTLiveStreamInfo.m
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/6/6.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import "MTTLiveStreamInfo.h"
+
+@implementation MTTLiveStreamInfo
+
+@end
