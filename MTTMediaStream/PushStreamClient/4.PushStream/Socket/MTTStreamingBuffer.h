87d591c49c4026d8c5496927045bcef6eb6a8e14
liuchuanan <waitwalker@163.com>
2019-06-06 11:41:08 +0800

    streaming buffer class init

---
+++ MTTMediaStream/PushStreamClient/4.PushStream/Socket/MTTStreamingBuffer.h
@@ -0,0 +1,17 @@
+//
+//  MTTStreamingBuffer.h
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/6/6.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTTStreamingBuffer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTTMediaStream/PushStreamClient/4.PushStream/Socket/MTTStreamingBuffer.m
@@ -0,0 +1,13 @@
+//
+//  MTTStreamingBuffer.m
+//  MTTMediaStream
+//
+//  Created by LiuChuanan on 2019/6/6.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import "MTTStreamingBuffer.h"
+
+@implementation MTTStreamingBuffer
+
+@end
