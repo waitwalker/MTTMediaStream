e3113beee6ad93bd5e5addf68b190eae2f0ef114
liuchuanan <waitwalker@163.com>
2019-06-06 11:56:26 +0800

    streaming buffer interface

--- MTTMediaStream/PushStreamClient/4.PushStream/Socket/MTTStreamingBuffer.h
+++ MTTMediaStream/PushStreamClient/4.PushStream/Socket/MTTStreamingBuffer.h
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "MTTAudioFrame.h"
+#import "MTTVideoFrame.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -20,7 +22,54 @@ typedef NS_ENUM(NSUInteger, MTTLiveBufferState) {
     MTTLiveBufferDeclines = 2,
 };
 
+@class MTTStreamingBuffer;
+@protocol MTTStreamingBufferDelegate <NSObject>
+
+@optional
+
+/**
+ 缓冲区状态回调
+
+ @param buffer 当前缓冲区
+ @param state 缓冲区状态
+ */
+- (void)streamingBuffer:(nullable MTTStreamingBuffer *)buffer bufferState:(MTTLiveBufferState)state;
+
+@end
+
 @interface MTTStreamingBuffer : NSObject
+
+@property(nullable, nonatomic, weak) id<MTTStreamingBufferDelegate>delegate;
+
+// current buffer frames
+@property (nonatomic, strong, readonly) NSMutableArray <MTTFrame *> *_Nonnull list;
+
+// buffer count max size default 1000
+@property (nonatomic, assign) NSUInteger maxCount;
+
+// 上次丢帧总数
+@property (nonatomic, assign) NSInteger lastDropFrames;
+
+
+/**
+ 添加帧到buffer list
+
+ @param frame 当前帧
+ */
+- (void)appendFrame:(nullable MTTFrame *)frame;
+
+/**
+ 弹出第一帧
+
+ @return 第一帧
+ */
+- (nullable MTTFrame *)popFirstFrame;
+
+/**
+ 删除所有帧
+ */
+- (void)removeAllFrame;
+
 
 @end
 
