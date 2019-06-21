6cee7554adc98c6911baf232c7c0d8cde9182cfc
liuchuanan <liuchuanan@etiantian.com>
2019-06-21 11:37:41 +0800

    add

---
+++ MTTMediaStream/MTTCallBackManager.h
@@ -0,0 +1,17 @@
+//
+//  MTTCallBackManager.h
+//  MTTMediaStream
+//
+//  Created by etiantian on 2019/6/21.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTTCallBackManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTTMediaStream/MTTCallBackManager.m
@@ -0,0 +1,13 @@
+//
+//  MTTCallBackManager.m
+//  MTTMediaStream
+//
+//  Created by etiantian on 2019/6/21.
+//  Copyright © 2019 waitwalker. All rights reserved.
+//
+
+#import "MTTCallBackManager.h"
+
+@implementation MTTCallBackManager
+
+@end
